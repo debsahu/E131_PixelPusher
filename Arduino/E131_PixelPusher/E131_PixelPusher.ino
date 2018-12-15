34e3b87436ca4d32a0ee7e4ceb3e937c26655cce
Debashish Sahu <debashish.sahu@gmail.com>
2018-12-14 22:20:49 -0500

    Slim and Fast
    - Use memcpy instead
    - Burden on sender to send in correct RGB order, must match color order of WS2812B LEDs

--- Arduino/E131_PixelPusher/E131_PixelPusher.ino
+++ Arduino/E131_PixelPusher/E131_PixelPusher.ino
@@ -3,7 +3,7 @@
 #include <ESPAsyncE131.h>        //https://github.com/forkineye/ESPAsyncE131
 #ifdef ESP8266
 #include <Hash.h>
-#include <ESPAsyncTCP.h> //https://github.com/me-no-dev/ESPAsyncTCP
+#include <ESPAsyncTCP.h>         //https://github.com/me-no-dev/ESPAsyncTCP
 #else
 #include <AsyncTCP.h>
 #endif
@@ -18,9 +18,9 @@
 #define HOSTNAME "E131PixelPusher"
 #define HTTP_PORT 80
 
-#define UNIVERSE 1                    // First DMX Universe to listen for
-#define UNIVERSE_COUNT 2              // Total number of Universes to listen for, starting at UNIVERSE max 7 for multicast and 12 for unicast
-#define ledCount 170 * UNIVERSE_COUNT // 170 LEDs per Universe
+#define UNIVERSE 1                        // First DMX Universe to listen for
+#define UNIVERSE_COUNT 7                  // Total number of Universes to listen for, starting at UNIVERSE max 7 for multicast and 12 for unicast
+uint16_t ledCount = UNIVERSE_COUNT * 170; // 170 LEDs per Universe
 
 // ESPAsyncE131 instance with UNIVERSE_COUNT buffer slots
 ESPAsyncE131 e131(UNIVERSE_COUNT);
@@ -28,17 +28,17 @@ AsyncWebServer server(HTTP_PORT);
 AsyncDNSServer dns;
 // DNSServer dns;
 
-NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(ledCount);
-NeoGamma<NeoGammaTableMethod> colorGamma;
+NeoEsp8266Dma800KbpsMethod dma = NeoEsp8266Dma800KbpsMethod(ledCount, 3);
+uint8_t *pixel = (uint8_t *)malloc(dma.getPixelsSize());
+
+#define SHOW_FPS_SERIAL //uncomment to see Serial FPS
+#ifdef SHOW_FPS_SERIAL
+uint64_t frameCt = 0;
+uint64_t PM = 0;
+float interval = 10 * 1000.0; // 10s
+#endif
 
 char update_html[] PROGMEM = R"=====(<!DOCTYPE html><html lang="en"><head><title>Firmware Update</title><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><meta name="viewport" content="width=device-width"><link rel="shortcut icon" type="image/x-icon" href="favicon.ico"></head><body><h3>Update Firmware</h3><br><form method="POST" action="/update" enctype="multipart/form-data"><input type="file" name="update"> <input type="submit" value="Update"></form></body></html>)=====";
-
-RgbColor GammaColor(uint8_t red, uint8_t green, uint8_t blue)
-{
-    RgbColor color_in(red, green, blue);
-    RgbColor color_out = colorGamma.Correct(color_in);
-    return color_out;
-}
 
 void setup()
 {
@@ -75,8 +75,8 @@ void setup()
         response->addHeader("Connection", "close");
         response->addHeader("Access-Control-Allow-Origin", "*");
         request->send(response);
-        if(shouldReboot) ESP.reset(); }, 
-        [](AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final) {
+        if(shouldReboot) ESP.reset(); },
+              [](AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final) {
           if (!filename.endsWith(".bin")) {
                 return;
             }
@@ -84,25 +84,19 @@ void setup()
                 Serial.printf("Update Start: %s\n", filename.c_str());
                 Update.runAsync(true);
                 if(!Update.begin((ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000)) {
-                #ifdef SERIALDEBUG
                 Update.printError(Serial);
-                #endif
                 }
             }
             if(!Update.hasError()){
                 if(Update.write(data, len) != len) {
-                #ifdef SERIALDEBUG
                 Update.printError(Serial);
-                #endif
                 }
             }
             if(final){
                 if(Update.end(true)) 
                 Serial.printf("Update Success: %uB\n", index+len);
                 else {
-                #ifdef SERIALDEBUG
                 Update.printError(Serial);
-                #endif
                 }
             } });
 
@@ -114,9 +108,12 @@ void setup()
         Serial.println(F("Listening for data..."));
     else
         Serial.println(F("*** e131.begin failed ***"));
-
-    strip.Begin();
-    strip.Show();
+    delay(1000);
+#ifdef SHOW_FPS_SERIAL
+    Serial.end();
+#endif
+    dma.Initialize();
+    memset(pixel, 0, sizeof(pixel));
 }
 
 void loop()
@@ -129,7 +126,8 @@ void loop()
         uint16_t universe = htons(packet.universe);
         uint8_t *data = packet.property_values + 1;
 
-        if (universe < UNIVERSE || universe > UNIVERSE_COUNT) return; //async will take care about filling the buffer
+        if (universe < UNIVERSE || universe > UNIVERSE_COUNT)
+            return; //async will take care about filling the buffer
 
         // Serial.printf("Universe %u / %u Channels | Packet#: %u / Errors: %u / CH1: %u\n",
         //               htons(packet.universe),                 // The Universe for this packet
@@ -138,18 +136,28 @@ void loop()
         //               e131.stats.packet_errors,               // Packet error counter
         //               packet.property_values[1]);             // Dimmer data for Channel 1
 
-        uint16_t len = 170; // (htons(packet.property_value_count) - 1) /3;
         uint16_t multipacketOffset = (universe - UNIVERSE) * 170; //if more than 170 LEDs (510 channels), client will send in next higher universe
-        if (ledCount <= multipacketOffset) return;
-        if (len + multipacketOffset > ledCount) len = ledCount - multipacketOffset;
-
-        for (uint16_t i = 0; i < len; i++)
-        {
-            uint16_t j = i * 3;
-            RgbColor pixelColor = GammaColor(data[j], data[j + 1], data[j + 2]);
-            strip.SetPixelColor(i + multipacketOffset, pixelColor);
-        }
-
-        strip.Show();
+        if (ledCount <= multipacketOffset)
+            return;
+        uint16_t len = (170 + multipacketOffset > ledCount) ? (ledCount - multipacketOffset) * 3 : 510;
+        memcpy(pixel + multipacketOffset * 3, data, len); // Burden on source to send in correct color order
     }
+
+    if (dma.IsReadyToUpdate())
+    {
+        memcpy(dma.getPixels(), pixel, dma.getPixelsSize());
+        dma.Update();
+#ifdef SHOW_FPS_SERIAL
+        frameCt++;
+#endif
+    }
+
+#ifdef SHOW_FPS_SERIAL
+    if (millis() - PM >= interval)
+    {
+        PM = millis();
+        Serial.printf("FPS: %.2f\n", interval / frameCt);
+        frameCt = 0;
+    }
+#endif
 }
